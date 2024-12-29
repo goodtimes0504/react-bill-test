a4cf33324d60a750dd0a7953b0c1bb2545068b94
goodtimes0504 <revolution_lc@outlook.com>
2024-12-29 17:32:44 +0800

    完成主题定制

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { createRoot } from "react-dom/client";
 // import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+//导入定制主题文件
+import "./theme.css";
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} future={{ v7_startTransition: true }} />
 );
