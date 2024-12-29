3001de1824dd521767e865f8d8dd37b68cc1e002
goodtimes0504 <revolution_lc@outlook.com>
2024-12-29 16:00:55 +0800

    别名路径配置

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
+
 // import "./index.css";
 import App from "./App.jsx";
 
+// import sum from "./test.js";
+import sum from "@/test.js";
+console.log(sum(1, 2));
 createRoot(document.getElementById("root")).render(<App />);
