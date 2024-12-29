3b65f761282d37b53955878e9fab662b4c1811a1
goodtimes0504 <revolution_lc@outlook.com>
2024-12-29 19:17:07 +0800

    tabbar切换功能实现

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,24 +1,58 @@
-import { Button } from "antd-mobile";
-import { useDispatch } from "react-redux";
-import { Outlet } from "react-router-dom";
-import { getBillList } from "@/store/modules/billStore";
+import { TabBar } from "antd-mobile";
 import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { getBillList } from "@/store/modules/billStore";
+import "./index.scss";
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline,
+} from "antd-mobile-icons";
+
+const tabs = [
+  {
+    key: "/month",
+    title: "月度账单",
+    icon: <BillOutline />,
+  },
+  {
+    key: "/new",
+    title: "记账",
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: "/year",
+    title: "年度账单",
+    icon: <CalculatorOutline />,
+  },
+];
 
 const Layout = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getBillList());
   }, [dispatch]);
+  //切换菜单 跳转路由
+  const navigate = useNavigate();
+  const switchRoute = (path) => {
+    navigate(path);
+    // console.log(path);
+  };
   return (
-    <div>
-      <Outlet />
-      Layout
-      {/* 测试全局生效样式 */}
-      <Button color="primary">测试全局</Button>
-      <div className="purple">
-        <Button color="primary">测试局部</Button>
+    <div className="layout">
+      <div className="container">
+        <Outlet />
+      </div>
+      <div className="footer">
+        <TabBar onChange={switchRoute}>
+          {tabs.map((item) => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
       </div>
     </div>
   );
 };
+
 export default Layout;
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,7 +3,7 @@ import Layout from "@/pages/Layout";
 import Month from "@/pages/Month";
 import New from "@/pages/New";
 import Year from "@/pages/Year";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 
 const router = createBrowserRouter(
   [
@@ -12,12 +12,16 @@ const router = createBrowserRouter(
       element: <Layout />,
       children: [
         {
-          index: true,
+          path: "month",
           element: <Month />,
         },
         {
           path: "year",
           element: <Year />,
+        },
+        {
+          index: true,
+          element: <Navigate to="/month" replace />,
         },
       ],
     },
